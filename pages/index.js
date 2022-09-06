8aad36201a254deee6c0dda9bf51d7284d5d5c12
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2022-09-06 14:21:44 +0200

    Init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Presentation from "../Views/Root/Presentation";
+
+function Index() {
+
+    return (
+        <Presentation />
+    );
+}
+
+export default Index;
